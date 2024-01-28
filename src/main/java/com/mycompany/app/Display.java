ad622322043cf706ee371b98387eeaf342ad49c0
Keir Nagy <redcat_24@outlook.com>
2024-01-28 01:01:41 -0600

    Changing to start new project

---
+++ src/main/java/com/mycompany/app/Display.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import javax.swing.*;
+
+public class Display extends JFrame {
+    JFrame frame;
+
+    public Display () {
+        frame = new Display();
+    }
+
+    
+}
